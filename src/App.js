e7aae255d3803b2ff248a9570bf0c04a015f6aa7
Steven Paulino <stevenpaulino1@hotmail.com>
2019-02-24 16:05:07 -0500

    Updated master with removePersonHandler

--- src/App.js
+++ src/App.js
@@ -16,26 +16,34 @@ class App extends Component {
   };
 
   mapThroughPersonsArray = () => {
-    return this.state.persons.map(person => (
+    return this.state.persons.map((person, index) => (
       <PersonGreeter
         key={person.id}
         person={person}
-        clicked={this.clickHandler}
+        name={person.name}
+        age={person.age}
+        clicked={()=>this.removePersonHandler(index)}
       />
     ));
   };
 
+  removePersonHandler = ( personIndex ) => {
+    const persons = [...this.state.persons];
+    persons.splice( personIndex, 1 );
+    this.setState( { persons: persons } );
+  }
+
   render() {
     return (
       <div className="app_heading">
-        <h1>Welcome to Person Greeter!</h1>
-        <button className="showButton" onClick={this.clickHandler}>
-          Show Persons
-        </button>
+        <h1 >Welcome to Person Greeter!</h1>
+        <button className="showButton" onClick={this.clickHandler}>Show Persons</button>
         {this.state.clicked && this.mapThroughPersonsArray()}
+
       </div>
     );
   }
 }
+
 
 export default App;
--- src/PersonGreeter.js
+++ src/PersonGreeter.js
@@ -5,7 +5,7 @@ const PersonGreeter = props => {
   return (
     <div className="personBorder">
       <h2>
-        Hi My name is {props.person.name} and I'm {props.person.age} years old
+        Hi My name is {props.name} and I'm {props.age} years old
       </h2>
       <button className="button" onClick={props.clicked}>
         I'm a Button!
